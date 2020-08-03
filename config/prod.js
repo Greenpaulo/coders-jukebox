daa8c3556e6499ccdb930b084b080a3c8c071e1d
Paul Bingham <greenpaulo@yahoo.co.uk>
2020-08-03 16:14:18 +0100

    Fixing deployment issues 3

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 // Production keys
 module.exports = {
   mongoURI: process.env.MONGO_URI,
-  YOUTUBEAPI_KEY: process.env.YOUTUBEAPI_KEY
+  // YOUTUBEAPI_KEY: process.env.YOUTUBEAPI_KEY
+  YOUTUBEAPI_KEY: 'AIzaSyDWTK5ebqXGHw3N4FSdxE_qO2v0LEs28Xo'
 };
