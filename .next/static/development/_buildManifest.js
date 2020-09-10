7c78b342d1bfd90952507a73dc15588d44d70b52
Paul Bingham <greenpaulo@yahoo.co.uk>
2020-09-10 14:55:38 +0100

    Untrack files in .gitignore

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/profile/[userId]":["static\u002Fchunks\u002Fpages\u002Fprofile\u002F[userId].js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
